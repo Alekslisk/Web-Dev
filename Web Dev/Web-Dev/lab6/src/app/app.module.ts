b51ff00a69ab942a262b8b8e3126a9aaf827ac00
Alekslisk <98632957+Alekslisk@users.noreply.github.com>
2023-03-08 21:26:35 +0600

    Add files via upload

---
+++ Web Dev/Web-Dev/lab6/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { HomeComponent } from './home/home.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './album-photos/album-photos.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    AlbumsComponent,
+    HomeComponent,
+    AlbumDetailComponent,
+    AlbumPhotosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
